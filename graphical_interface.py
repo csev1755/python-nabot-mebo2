7e5825f41500d557b77ede6fee47746c7f57a290
csev1755 <charlie.seversin@gmail.com>
2025-04-02 21:32:15 -0500

    rename send_robot_to_goal

--- graphical_interface.py
+++ graphical_interface.py
@@ -30,7 +30,7 @@ class GraphicalInterface():
 
         if 'robot_init_pos' in self.kwargs:
             self.logger.info('Sending robot to center.')
-            self.robot_ctrl.send_robot_to_goal(goal=kwargs['robot_init_pos'])
+            self.robot_ctrl.set_joint_positions(goal=kwargs['robot_init_pos'])
             self.logger.info('Robot centered.')
 
         np.set_printoptions(precision=2)
